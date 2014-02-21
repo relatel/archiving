de50ffd977c433f370163d733e4214e45d16185d
Harry Vangberg <harry@vangberg.name>
2014-02-21 10:45:11 +0100

    format

--- test/archive_table_test.rb
+++ test/archive_table_test.rb
@@ -1,24 +1,24 @@
 require 'test_helper'
- 
+
 class ArchiveTableTest < ActiveSupport::TestCase
-  def setup
+  setup do
     Post.delete_all
     Post::Archive.delete_all
   end
 
-  def test_creates_archive_model
+  test "creates_archive_model" do
     assert Post::Archive < Post
   end
 
-  def test_shorthand_for_archive_model
+  test "shorthand for archive model" do
     assert_equal Post::Archive, Post.archive
   end
 
-  def test_add_archive_suffix_to_table_name
+  test "add archive suffix to table name" do
     assert_equal "posts_archive", Post::Archive.table_name
   end
 
-  def test_with_archive_unions_results_from_both_tables
+  test "with_archive unions results from both tables" do
     p1 = Post.create!(title: "Post 1", tag: "news")
     p2 = Post.create!(title: "Post 2", tag: "misc")
     a1 = Post::Archive.create!(title: "Archive 1", tag: "news")

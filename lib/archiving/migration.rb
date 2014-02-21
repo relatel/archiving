9ebafec8844f6015d863d060e3f8eeec95c3acc9
Harry Vangberg <harry@vangberg.name>
2014-02-21 10:11:32 +0100

    empty migration module

---
+++ lib/archiving/migration.rb
@@ -0,0 +1,13 @@
+require "active_support/concern"
+
+module Archiving
+  module Migration
+    extend ActiveSupport::Concern
+
+    included do
+    end
+
+    module ClassMethods
+    end
+  end
+end

668c8f7ca12b4dce5ec01d8a374e5e837350ca1e
Michael Kyed <michael@kyed.dk>
2016-11-03 13:44:12 +0100

    blocked it

--- test/dummy/db/migrate/20161103100644_create_log_other_archive.rb
+++ test/dummy/db/migrate/20161103100644_create_log_other_archive.rb
@@ -1,26 +1,24 @@
 class CreateLogOtherArchive < ActiveRecord::Migration
   def up
-    set_connection
-    create_table :log_others_archive do |t|
-      t.references :post
-      t.string :note
+    set_connection do
+      create_table :log_others_archive do |t|
+        t.references :post
+        t.string :note
+      end
     end
-    restore_connection
   end
 
   def down
-    set_connection
-    drop_table :log_others_archive
-    restore_connection
+    set_connection do
+      drop_table :log_others_archive
+    end
   end
 
   def set_connection
-    @connection_was = @connection
+    connection_was = @connection
     @connection = ActiveRecord::Base.establish_connection(:"other_#{Rails.env}").connection
-  end
-
-  def restore_connection
-    @connection = @connection_was
+    yield
+    @connection = connection_was
     ActiveRecord::Base.remove_connection
     ActiveRecord::Base.establish_connection
   end

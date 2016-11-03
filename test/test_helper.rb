406ac792b6fdebfcc0fd28d209bae2ca9f21907d
Michael Kyed <michael@kyed.dk>
2016-11-03 12:28:33 +0100

    Added focus and mock'er

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,8 @@ ENV["RAILS_ENV"] = "test"
 
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "rails/test_help"
+require "rr"
+require "minitest/focus"
 
 Rails.backtrace_cleaner.remove_silencers!
 
